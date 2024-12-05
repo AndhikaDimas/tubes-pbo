2874aa9724de8e4ca1e13d349b84bfae0a700353
Arifsyafarrudin <arifsyafarrudin659@gmail.com>
2024-12-05 13:28:44 +0700

    delete

--- tanaman.php
+++ tanaman.php
@@ -5,7 +5,6 @@ echo "----------------";
 echo "nasi putih";
 echo "kecap";
 echo "2 sdm minyak goreng";
-<<<<<<< HEAD
 echo "1 batang daun bawang";
 echo "70g wortel, kupas, potong dadu kecil";
 echo "1siung bawang putih, iris halus";
@@ -14,9 +13,4 @@ echo "1siung bawang putih, iris halus";
 echo "Panaskan minyak, tumis bawang putih, bawang merah hingga harum.";
 echo "Tambahkan nasi, Royco Kaldu Ayam, dan Bango Kecap Manis, aduk rata";
 echo "lalu makan";
-=======
-echo "";
 
-echo "ambil wajan atau panci";
-echo "taro wajan atau panci di atas kompor";
->>>>>>> c8ccbe4e7b6c33bd1e0b41373748dcedb74b3409
