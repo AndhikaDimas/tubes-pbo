da4e70c303bc670c4038dae1b6249825af282f81
Arifsyafarrudin <arifsyafarrudin659@gmail.com>
2024-12-05 13:22:07 +0700

    proses

---
+++ nasgor.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "cara membuat nasi goreng";
+echo "----------------";
+echo "nasi putih";
+echo "kecap";
+echo "2 sdm minyak goreng";
+echo "1 batang daun bawang";
+echo "70g wortel, kupas, potong dadu kecil";
+echo "1siung bawang putih, iris halus";
+
+//arif
+echo "Panaskan minyak, tumis bawang putih, bawang merah hingga harum.";
+echo "Tambahkan nasi, Royco Kaldu Ayam, dan Bango Kecap Manis, aduk rata";
+echo "lalu makan";
